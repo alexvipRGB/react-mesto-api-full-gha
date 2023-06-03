87ab40fb616346687e1701f5508819731522f499
alexvipRGB <93926733+alexvipRGB@users.noreply.github.com>
2023-06-03 14:44:32 +0300

    Обновление 3

--- backend/utils/secretKey.js
+++ backend/utils/secretKey.js
@@ -1,3 +1,3 @@
-const crypto = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEifQ.BQmWM1mXBfpTw_Tv-yR3qodI0OoRmrm3Tlz6ZR60Yi4';
+const crypto = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEyMzQ1Njc4OTAifQ._aG0ukzancZqhL1wvBTJh8G8d3Det5n0WKcPo5C0DCY';
 
 module.exports = crypto;
