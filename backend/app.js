cf515a9d57d595fc846d02440e31d4cf41c4b6e9
alexvipRGB <93926733+alexvipRGB@users.noreply.github.com>
2023-06-03 15:07:32 +0300

    Обновление

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const { errors } = require('celebrate');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
@@ -17,25 +16,13 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
-app.use(
-  cors({
-    origin: [
-      'http://localhost:3001',
-      'http://alex.rgb.yandex.nomoredomains.rocks',
-      'https://alex.rgb.yandex.nomoredomains.rocks',
-    ],
-    credentials: true,
-    maxAge: 60,
-  }),
-);
+app.use(cors);
 
 app.use(helmet());
 
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
 app.use(requestLogger);
-
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.get('/crash-test', () => {
   setTimeout(() => {
