f89f5ba78788f2291d220b77d2327d02909396b9
alexvipRGB <93926733+alexvipRGB@users.noreply.github.com>
2023-06-03 19:56:14 +0300

    Обновление

---
+++ frontend/utils/auth.js
@@ -0,0 +1,55 @@
+class Auth {
+  constructor({ url, headers }) {
+    this._url = url;
+    this._headers = headers;
+  }
+
+  _handleResponse(res) {
+    if (res.ok) {
+      return res.json();
+    } else {
+      return Promise.reject(`Ошибка: ${res.status}`);
+    }
+  }
+
+  registerUser(email, password) {
+    return fetch(`${this._url}/signup`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    })
+      .then(this._handleResponse)
+  }
+
+  loginUser(email, password) {
+    return fetch(`${this._url}/signin`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: "include",
+      body: JSON.stringify({ email, password }),
+    })
+      .then(this._handleResponse)
+  }
+
+  getToken(token) {
+    return fetch(`${this._url}/users/me`, {
+      method: 'GET',
+      credentials: "include",
+      headers: {
+        Accept: "application/json",
+       "Content-Type": "application/json",
+      },
+    })
+    .then(this._handleResponse)
+  }
+}
+
+const auth = new Auth({
+  url: 'https://api.mesto.alex.nomoredomains.rocks',
+});
+
+export default auth;
