32035e405d2fcfbb56d027372e57787f18cc1ebb
alexvipRGB <93926733+alexvipRGB@users.noreply.github.com>
2023-06-05 13:51:06 +0300

    Add files via upload

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,26 +2,26 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const UnauthorizedError = require('../errors/UnauthorizedError');
-const secretKey = require('../utils/secretKey');
 
 const authMiddleware = (req, res, next) => {
-  const token = req.cookies.jwt;
-  let payload;
-  if (!token) {
+  const { authorization } = req.headers;
+  const bearer = 'Bearer ';
+
+  if (!authorization || !authorization.startsWith(bearer)) {
     throw new UnauthorizedError('Необходима авторизация');
   }
 
+  const token = authorization.replace(bearer, '');
+  let payload;
+
   try {
-    payload = jwt.verify(
-      token,
-      NODE_ENV === 'production' ? JWT_SECRET : secretKey,
-    );
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     throw new UnauthorizedError('Необходима авторизация');
   }
   req.user = payload;
 
-  next();
+  return next();
 };
 
 module.exports = authMiddleware;
