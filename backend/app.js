0216b88332fa4555d07673767f91eebec48084cb
alexvipRGB <93926733+alexvipRGB@users.noreply.github.com>
2023-06-06 22:10:24 +0300

    Add files via upload

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,15 @@ app.use(
     maxAge: 60,
   }),
 );
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE',
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 app.use(helmet());
 
 app.use(cookieParser());
