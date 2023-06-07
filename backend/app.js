22dca753408965c8d6343cc671d56215bcf2dab3
alexvipRGB <93926733+alexvipRGB@users.noreply.github.com>
2023-06-07 14:17:32 +0300

    Add files via upload

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,11 @@
-require('dotenv').config();
+const path = require('path');
 const express = require('express');
-const cors = require('cors');
-const { errors } = require('celebrate');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const router = require('./routes/index');
 const validationErrors = require('./utils/validError');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
-const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -17,40 +14,15 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
-app.use(
-  cors({
-    origin: [
-      'http://localhost:3001',
-      'http://alex.rgb.yandex.nomoredomains.rocks',
-      'https://alex.rgb.yandex.nomoredomains.rocks',
-      'http://api.mesto.alex.nomoredomains.rocks',
-      'https://api.mesto.alex.nomoredomains.rocks',
-    ],
-    credentials: true,
-    maxAge: 60,
-  }),
-);
-
-app.use('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-
 app.use(helmet());
 
-app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
 
-app.use(requestLogger);
-
+app.use(bodyParser.json());
 app.use(router);
-app.use(auth);
-app.use(errorLogger);
 
 app.use(validationErrors);
-app.use(errors());
 
 app.listen(PORT);
