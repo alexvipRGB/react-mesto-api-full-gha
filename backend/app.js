7e3c814de4958b50c421b754a7b3d84650bf1602
alexvipRGB <93926733+alexvipRGB@users.noreply.github.com>
2023-06-07 16:48:33 +0300

    Add files via upload

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,15 @@
-const path = require('path');
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const { errors } = require('celebrate');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const router = require('./routes/index');
 const validationErrors = require('./utils/validError');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -14,15 +18,43 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+app.use(
+  cors({
+    origin: [
+      'http://localhost:3001',
+      'http://alex.rgb.yandex.nomoredomains.rocks',
+      'https://alex.rgb.yandex.nomoredomains.rocks',
+      'http://api.mesto.alex.nomoredomains.rocks',
+      'https://api.mesto.alex.nomoredomains.rocks',
+    ],
+    credentials: true,
+    maxAge: 60,
+  }),
+);
+
+app.use('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use(helmet());
 
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.json());
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(requestLogger);
 
 app.use(bodyParser.json());
 app.use(router);
+app.use(auth);
+app.use(errorLogger);
 
 app.use(validationErrors);
+app.use(errors());
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const NotFoundError = require('../errors/NotFoundError');
 
 const router = express.Router();
+const crashTest = require('./crashTest');
 const userRouter = require('./users');
 const cardRouter = require('./cards');
 
+router.use('/crash-test', crashTest);
 router.use(userRouter);
 router.use(cardRouter);
-router.use((req, res, next) => {
-  next(new NotFoundError('Маршрут не найден'));
+router.use(() => {
+  throw new NotFoundError('Маршрут не найден');
 });
 
 module.exports = router;
